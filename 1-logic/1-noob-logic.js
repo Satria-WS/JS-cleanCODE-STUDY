13e2db481d1dd2cf9d468555db726c0b10d58386
Satria-WS <extencezone02@gmail.com>
2022-06-13 03:48:17 +0700

    start

---
+++ 1-logic/1-noob-logic.js
@@ -0,0 +1,9 @@
+function toCalculating(n) {
+  if (n < 0) {
+    return Math.abs(n);
+  } else {
+    return n.toString();
+  }
+}
+console.log(typeof(toCalculating("-4")));
+console.log(toCalculating(9));
